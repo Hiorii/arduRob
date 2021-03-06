2de7c4bb33fc5a2a996b81084033a7fd1c6d8b52
Bartosz Janiak <janiak.bartosz92@gmail.com>
2021-03-06 17:35:31 +0100

    RWD fix. Google sign in fix.

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
 export const GOOGLE_URL = (process.env.NODE_ENV === 'production') ? '/auth/google/callback' : 'http://localhost:8000/auth/google/callback';
+
